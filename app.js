54616e8c58cc0a9d4324676a2661af48a5e2e1e9
Caroline Knote <cj.knote@gmail.com>
2022-06-10 19:33:42 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+/**
+ * An app interface for playing the game Fae's Anatomy
+ * 
+ * Features:
+ * 
+ * Features to be added: character summaries (variable amounts)
+ * saved database/code 
+ * status bar
+ * 
+ * Creator: Caroline Knote 
+ * template concept by Jasper LaFleur 
+ * 
+ */
+
